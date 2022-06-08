4351466191c498634c1816945284284e0180be57
Artem <armrt@mail.ru>
2022-06-09 00:32:12 +0300

    Установил "PropTypes"

--- src/Components/Form/components/Input/Input.js
+++ src/Components/Form/components/Input/Input.js
@@ -1,14 +1,17 @@
+import TextField from '@mui/material/TextField';
 import './Input.css';
 
 export const Input = ({ text, onChange }) => {
   return (
-    <input
+    <TextField
       className="input"
       type="text"
       placeholder="  Your message..."
       value={text}
       onChange={onChange}
       data-testid="input"
+      variant="outlined"
+      style={{ marginRight: '10px' }}
     />
   );
 };
