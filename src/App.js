4f0062f540bada426f698c839518e4a54ad251d1
Artem <armrt@mail.ru>
2022-06-15 23:03:59 +0300

    Добавил логику разделения чатов

--- src/App.js
+++ src/App.js
@@ -1,16 +1,48 @@
-import React, { useState } from 'react';
+import React, { useMemo, useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { ChatList } from './Components/ChatList/ChatList';
 import { Header } from './Components/Header';
 import { Main } from './pages/Main';
 import { Profile } from './pages/Profile';
 import { ChatPage } from './pages/ChatPages';
+import { nanoid } from 'nanoid';
+
+// const defaultChats = [
+//   {
+//     id: 1,
+//     name: 'default',
+//   },
+// ];
+
+const defaultMessages = {
+  default: [],
+};
 
 export const App = () => {
-  const [chats, setChats] = useState([]);
+  const [messages, setMessages] = useState(defaultMessages);
+
+  const chats = useMemo(
+    () =>
+      Object.keys(messages).map((chat) => ({
+        id: nanoid(),
+        name: chat,
+      })),
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [Object.keys(messages).length, messages]
+  );
 
   const onAddChat = (newChat) => {
-    setChats([...chats, newChat]);
+    setMessages({
+      ...messages,
+      [newChat.name]: [],
+    });
+  };
+
+  const onAddMessage = (chatId, newMessage) => {
+    setMessages({
+      ...messages,
+      [chatId]: [...messages[chatId], newMessage],
+    });
   };
 
   return (
@@ -26,7 +58,14 @@ export const App = () => {
             />
             <Route
               path=":chatId"
-              element={<ChatPage chats={chats} onAddChat={onAddChat} />}
+              element={
+                <ChatPage
+                  chats={chats}
+                  onAddChat={onAddChat}
+                  messages={messages}
+                  onAddMessage={onAddMessage}
+                />
+              }
             />
           </Route>
         </Route>
--- src/pages/ChatPages.js
+++ src/pages/ChatPages.js
@@ -1,28 +1,29 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { Form } from '../Components/Form';
 import { MessageList } from '../Components/MessagesList';
 import { AUTHOR } from '../data';
 import style from '../App.module.css';
 import { ChatList } from '../Components/ChatList/ChatList';
+import { Navigate, useParams } from 'react-router-dom';
 
-const startMessages = [
-  {
-    author: AUTHOR.user,
-    text: 'Hello!',
-  },
-];
+// const startMessages = [
+//   {
+//     author: AUTHOR.user,
+//     text: 'Hello!',
+//   },
+// ];
 
-export const ChatPage = ({ chats, onAddChat }) => {
-  const [messages, setMessages] = useState(startMessages);
-
-  const addMessage = (newMessage) => {
-    setMessages([...messages, newMessage]);
-  };
+export const ChatPage = ({ chats, onAddChat, messages, onAddMessage }) => {
+  const { chatId } = useParams();
 
   useEffect(() => {
-    if (messages[messages.length - 1].author === AUTHOR.user) {
+    if (
+      chatId &&
+      messages[chatId]?.length > 0 &&
+      messages[chatId][messages[chatId].length - 1].author === AUTHOR.user
+    ) {
       const timeout = setTimeout(() => {
-        addMessage({
+        onAddMessage(chatId, {
           author: AUTHOR.bot,
           text: 'Hello Artem!',
         });
@@ -33,14 +34,24 @@ export const ChatPage = ({ chats, onAddChat }) => {
       };
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [messages]);
+  }, [chatId, messages]);
+
+  const handleAddMessage = (mesasge) => {
+    if (chatId) {
+      onAddMessage(chatId, mesasge);
+    }
+  };
+
+  if (chatId && !messages[chatId]) {
+    return <Navigate to="/chats" replace />;
+  }
 
   return (
     <>
       <ChatList chats={chats} onAddChat={onAddChat} />
       <div className={style.flud}>
-        <MessageList messages={messages} />
-        <Form addMessage={addMessage} />
+        <MessageList messages={messages[chatId]} />
+        <Form addMessage={handleAddMessage} />
       </div>
     </>
   );
