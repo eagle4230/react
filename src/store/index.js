809323c96211ae91646c192c73da5397168fa8fe
Artem <armrt@mail.ru>
2022-06-27 00:43:16 +0300

    Применил "redux-persist" для сообщений

--- src/store/index.js
+++ src/store/index.js
@@ -1,17 +1,31 @@
 import { compose, createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
+
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
 import { profileReducer } from './profile/reducer';
 import { messageReducer } from './messages/reducer';
 
 export const composeEnhancers =
   window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
+const persistConfig = {
+  key: 'root',
+  storage,
+  blacklist: ['profile'],
+}
+
 const rootReducer = combineReducers({
   profile: profileReducer,
   messages: messageReducer,
 });
 
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 export const store = createStore(
-  rootReducer,
+  persistedReducer,
   composeEnhancers(applyMiddleware(thunk))
 );
+
+export const persistor = persistStore(store);
