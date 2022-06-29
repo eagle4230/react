761c4e2b344847c699e5321c569e9d8b20e8ac76
Artem <armrt@mail.ru>
2022-06-30 00:43:10 +0300

    Имитировал долгую загрузку заголовков статей

--- src/pages/Articles.js
+++ src/pages/Articles.js
@@ -1,21 +1,38 @@
-import { useState } from "react";
-import { useEffect } from "react";
-import { api } from "../data";
+import { useState } from 'react';
+import { useEffect } from 'react';
+import { api } from '../data';
 
 export const Articles = () => {
   const [articles, setArticles] = useState([]);
+  const [loadign, setLoadigin] = useState(false);
+  const [error, setError] = useState('');
 
   useEffect(() => {
-    fetch(api)
-      .then((res) => res.json())
-      .then((data) => setArticles(data));
+    setLoadigin(true);
+    setError('');
+
+    setTimeout(() => {
+      fetch(api)
+        .then((res) => res.json())
+        .then((data) => setArticles(data))
+        .catch((err) => setError(err.message))
+        .finally(() => setLoadigin(false));
+    }, 1000)
   }, []);
+
   return (
     <>
       <h2>Articles</h2>
-      <ul>
-        {articles.map(article => <li key={article.id}>{article.title}</li>)}
-      </ul>
+      {loadign && <p>Loading...</p>}
+      {!loadign && (
+        <ul>
+          {articles.map((article) => (
+            <li key={article.id}>{article.title}</li>
+          ))}
+        </ul>
+      )}
+
+      {error && <p style={{ color: 'red' }}>{error}</p>}
     </>
   );
 };
